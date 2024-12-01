ebf18753a89e14746a3d711c946e2a687a9bb904
Piter Capistrano <pitercapistrano@gmail.com>
2024-12-01 14:05:56 -0300

    Crinado Constantes

---
+++ lib/common/constants/app_colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+
+  static const Color lightGreenOne = Color(0xff63B5AF);
+  static const Color lightGreenTwo = Color(0xFF438883);
+  static const List<Color> greenGradient = [
+    Color(0xff63B5AF),
+    Color(0xFF438883),
+  ];
+  static const Color white = Color(0xFFFFFFFF);
+}
