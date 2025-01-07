d4c99ea184028cdadeb1dd7691023646cb275520
Piter Capistrano <pitercapistrano@gmail.com>
2025-01-07 09:59:46 -0300

    Criação do Locator através do get_it

---
+++ lib/locator.dart
@@ -0,0 +1,17 @@
+import 'package:get_it/get_it.dart';
+import 'package:money_map/common/models/user_model.dart';
+import 'package:money_map/features/sign_in/sign_in_controller.dart';
+import 'package:money_map/features/sign_up/sign_up_controller.dart';
+import 'package:money_map/services/auth_service.dart';
+import 'package:money_map/services/mock_auth_service.dart';
+
+final locator = GetIt.instance;
+
+void setupDependencies() {
+  locator.registerLazySingleton<AuthService>(() => MockAuthService());
+
+  locator.registerFactory<SignInController>(
+      () => SignInController(locator.get<AuthService>()));
+  locator.registerFactory<SignUpController>(
+      () => SignUpController(locator.get<AuthService>()));
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:money_map/app.dart';
+import 'package:money_map/locator.dart';
 
 void main() {
+  setupDependencies();
   runApp(const App());
 }
