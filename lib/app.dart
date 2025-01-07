98c52690664bde5b115f2d68b49b097dae00b18a
Piter Capistrano <pitercapistrano@gmail.com>
2025-01-06 21:47:38 -0300

    Fluxo de Navegação de Telas

--- lib/app.dart
+++ lib/app.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:money_map/common/constants/routes.dart';
 import 'package:money_map/common/themes/default_theme.dart';
 import 'package:money_map/features/onboarding/onboarding_page.dart';
+import 'package:money_map/features/sign_in/sign_in.dart';
 import 'package:money_map/features/sign_up/sign_up.dart';
 import 'package:money_map/features/splash/splash_page.dart';
 
@@ -17,6 +18,7 @@ class App extends StatelessWidget {
         NamedRoute.initial: (context) => const OnboardingPage(),
         NamedRoute.splash: (context) => const SplashPage(),
         NamedRoute.signUp: (context) => const SignUp(),
+        NamedRoute.signIn: (context) => const SignIn(),
       },
     );
   }
